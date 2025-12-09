a4ae28c0ccfc87b975b697ece05012cef7b623ec
A. H. M. Faisal <ahmfaisal10@gmail.com>
2025-12-09 23:02:43 +0600

    feat(auth): auth routes added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
-import type { Router } from 'express';
-import express from 'express';
+import { Router } from 'express';
+import { authRoutes } from '@/modules/auth/auth.routes';
 
-const router = express.Router();
+const router = Router();
 
-const routers: { path: string; router: Router }[] = [];
+const routers: { path: string; router: Router }[] = [
+	{
+		router: authRoutes,
+		path: '/auth',
+	},
+];
 
 routers.forEach((route) => {
 	router.use(route.path, route.router);
